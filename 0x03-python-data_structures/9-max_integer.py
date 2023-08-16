96c887fc5dec5ef5a2f0c4992189da63b38eaeff
Teminijesu Olaleye <holarxmanuel@gmail.com>
2023-08-16 01:56:50 +0000

    gun

---
+++ 0x03-python-data_structures/9-max_integer.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+def max_integer(my_list=[]):
+    lent = len(my_list)
+
+    if lent == 0:
+        return None
+
+    max_int = my_list[0]
+
+    for i in range(1, lent):
+        if my_list[i] > max_int:
+            max_int = my_list[i]
+
+    return max_int
