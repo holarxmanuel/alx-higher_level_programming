e8ef8222cc803168c35a59a0b802a6331be9160c
Teminijesu Olaleye <holarxmanuel@gmail.com>
2023-08-16 00:43:19 +0000

    adjust

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -1,21 +1,20 @@
-#!/usr/bin/python3
+#!/usr/bin/pytho
 def print_reversed_list_integer(my_list=[]):
-    list_length = 0
-    for _ in my_list:
-        list_length += 1
-
-    idx = list_length - 1
-    while idx >= 0:
-        number = my_list[idx]
-        
-        # Convert and print the number without using str.format() or casting to string
+    for idx in range(len(my_list)):
         digit_list = []
-        while number > 0:
-            digit = number % 10
-            digit_list.insert(0, chr(ord('0') + digit))
-            number //= 10
-        
-        integer_str = ''.join(digit_list)
-        print(integer_str)
+        number = my_list[-(idx + 1)]
 
-        idx -= 1
+        if number == 0:
+            digit_list.append(chr(ord('0')))
+        else:
+            while number > 0:
+                digit = number % 10
+                digit_list.insert(0, chr(ord('0') + digit))
+                number //= 10
+
+        integer_str = ''.join(digit_list)
+        print("{}".format(integer_str))
+
+# Example usage:
+my_list = [5, 10, 15, 20, 25]
+print_reversed_list_integer(my_list)n
