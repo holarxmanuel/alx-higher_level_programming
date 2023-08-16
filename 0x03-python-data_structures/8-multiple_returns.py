507f68e6e09a04260c1b82f5f12e75356e806b83
Teminijesu Olaleye <holarxmanuel@gmail.com>
2023-08-16 01:44:51 +0000

    gat

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,12 +1,8 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
-    if len(sentence) == 0:
+    if not sentence:
         first_char = None
     else:
         first_char = sentence[0]
     
-    return len(sentence), first_char
-
-# Example usage:
-result = multiple_returns("Hello, world!")
-print(result)  # Output: (13, 'H')
+    return len(sentence), first_char)
