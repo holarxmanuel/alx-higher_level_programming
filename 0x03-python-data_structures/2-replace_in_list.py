bd54046e1e3ad764a386bf4f03bfbe67f82982bc
Teminijesu Olaleye <holarxmanuel@gmail.com>
2023-08-16 00:35:44 +0000

    khi

--- 0x03-python-data_structures/2-replace_in_list.py
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -1,11 +1,8 @@
 #!/usr/bin/python3
 def replace_in_list(my_list, idx, element):
-    list_length = 0
-    for _ in my_list:
-        list_length += 1
-
-    if idx < 0 or idx >= list_length:
+    if idx < 0 or idx >= len(my_list):
         return my_list
-    
+
     my_list[idx] = element
+
     return my_list
