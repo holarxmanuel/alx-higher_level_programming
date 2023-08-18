2cb53aca63f36455afd815a568676f645be82ebc
Teminijesu Olaleye <holarxmanuel@gmail.com>
2023-08-18 18:26:59 +0000

    gauu

--- 0x04-python-more_data_structures/100-weight_average.py
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -1,18 +1,10 @@
 #!/usr/bin/python3
 
-"""
-Calculates the weighted average of integers in the list of tuples.
-
-:param my_list: List of tuples containing integer score and weight
-:return: Weighted average, or 0 if the list is empty
-"""
-
 def weight_average(my_list=[]):
     if not my_list:
         return 0
 
-    total_score = 0
-    total_weight = 0
+    total_score, total_weight = 0, 0
 
     for score, weight in my_list:
         total_score += score * weight
