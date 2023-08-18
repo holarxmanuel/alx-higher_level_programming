df3e1c1fe96f9d2cf15b519ac3ab223569df02f8
Teminijesu Olaleye <holarxmanuel@gmail.com>
2023-08-18 16:01:22 +0000

    check

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -11,10 +11,3 @@ def search_replace(my_list, search, replace):
     """
     new_list = [replace if item == search else item for item in my_list]
     return new_list
-
-if __name__ == "__main__":
-    # Example usage
-    original_list = [1, 2, 3, 2, 4, 5, 2]
-    new_element = 9
-    replacement_list = search_replace(original_list, 2, new_element)
-    print(replacement_list)
