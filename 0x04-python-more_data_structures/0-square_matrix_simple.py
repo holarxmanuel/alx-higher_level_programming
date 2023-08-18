1b57b8a6f8ae3c419ca5aa29de54fbe54b541280
Teminijesu Olaleye <holarxmanuel@gmail.com>
2023-08-18 15:29:59 +0000

    gijn

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,21 +1,15 @@
 #!/usr/bin/python3
 
 def square_matrix_simple(matrix=[]):
-    # Create a new matrix to store squared values
     squared_matrix = []
-    
-    # Iterate through each row in the input matrix
+
     for row in matrix:
-        # Create a new row for the squared values
         squared_row = []
-        
-        # Iterate through each element in the row
+
         for value in row:
-            # Square the value and append it to the squared row
             squared_row.append(value ** 2)
-        
-        # Append the squared row to the squared matrix
+
         squared_matrix.append(squared_row)
-    
+
     return squared_matrix
 
