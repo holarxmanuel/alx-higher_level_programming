c47b552a1c96f060736f56346c5f5e1ed9573c6f
Teminijesu Olaleye <holarxmanuel@gmail.com>
2023-08-18 15:44:58 +0000

    hope

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,15 +1,14 @@
 #!/usr/bin/python3
 
 def square_matrix_simple(matrix=[]):
-    squared_matrix = []
+    """
+    Computes the square value of all integers in a matrix.
 
+    :param matrix: A 2D array of integers
+    :return: A new matrix with each value squared
+    """
+    squared_result = []
     for row in matrix:
-        squared_row = []
-
-        for value in row:
-            squared_row.append(value ** 2)
-
-        squared_matrix.append(squared_row)
-
-    return squared_matrix
-
+        squared_row = list(map(lambda x: x**2, row))
+        squared_result.append(squared_row)
+    return squared_result
