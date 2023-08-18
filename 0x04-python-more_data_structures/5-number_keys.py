7064e9dff0c288dd0831ccf2b9c20d3de90aef26
Teminijesu Olaleye <holarxmanuel@gmail.com>
2023-08-18 16:18:17 +0000

    random

---
+++ 0x04-python-more_data_structures/5-number_keys.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+def number_keys(a_dictionary):
+    """
+    Returns the number of keys in a dictionary.
+
+    :param a_dictionary: The input dictionary
+    :return: The number of keys in the dictionary
+    """
+    return len(a_dictionary)
