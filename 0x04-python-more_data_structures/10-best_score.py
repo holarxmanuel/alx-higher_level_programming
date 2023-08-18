908b2f775954932ba6506e0e52475f725bdf4f31
Teminijesu Olaleye <holarxmanuel@gmail.com>
2023-08-18 17:34:13 +0000

    allo

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,12 +1,13 @@
 #!/usr/bin/python3
 
-def best_score(a_dictionary):
-    """
-    Returns the key with the biggest integer value in a dictionary.
+"""
+Returns the key with the biggest integer value in a dictionary.
 
-    :param a_dictionary: The input dictionary
-    :return: The key with the biggest integer value, or None if no score is found
-    """
+:param a_dictionary: The input dictionary
+:return: The key with the biggest integer value, or None if no score is found
+"""
+
+def best_score(a_dictionary):
     if not a_dictionary:
         return None
     max_key = max(a_dictionary, key=a_dictionary.get)
