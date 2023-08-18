34b4a3375fac591534883dd2f1d509b0c827e78a
Teminijesu Olaleye <holarxmanuel@gmail.com>
2023-08-18 15:16:54 +0000

    vat

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -19,9 +19,5 @@ def square_matrix_simple(matrix=[]):
 
     return squared_matrix
 
-if __name__ == "__main__":
-    # Example usage
-    matrix = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
-    result = square_matrix_simple(matrix)
-    for row in result:
-        print(row)
+if __name__ == "__main__"
+   
