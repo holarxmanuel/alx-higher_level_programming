f07c848df2d363efe61623fac7d9b4c9f97ad057
Teminijesu Olaleye <holarxmanuel@gmail.com>
2023-08-18 17:55:29 +0000

    har

--- 0x04-python-more_data_structures/7-update_dictionary.py
+++ 0x04-python-more_data_structures/7-update_dictionary.py
@@ -1,11 +1,5 @@
 #!/usr/bin/python3
 
 def update_dictionary(a_dictionary, key, value):
-    """
-    Replaces or adds key/value pair in a dictionary.
-
-    :param a_dictionary: The input dictionary
-    :param key: The key to replace or add
-    :param value: The value to assign to the key
-    """
+    """Replace or add key/value pairs in a dictionary."""
     a_dictionary[key] = value
