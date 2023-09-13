382a0d37c43e3dc923c25a0dec48fbc281b813c9
Teminijesu Olaleye <holarxmanuel@gmail.com>
2023-09-12 21:59:20 -0700

    rou&edgecleanright

---
+++ 0x0B-python-input_output/6-load_from_json_file.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""
+Defines a function to load an object from a JSON file.
+"""
+
+import json
+
+def load_from_json_file(filename):
+    """
+    Load an object from the specified JSON file.
+
+    :param filename: The name of the JSON file to load the object from.
+    :return: The Python object loaded from the JSON file.
+    """
+    with open(filename, 'r', encoding='utf-8') as file:
+        return json.load(file)
