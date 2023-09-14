2cb5b52808d85ef4b4649dff8bd044991db2af00
Teminijesu Olaleye <holarxmanuel@gmail.com>
2023-09-14 15:57:01 -0700

    rural

---
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""Base module"""
+
+
+class Base:
+    """Base class for managing id attribute"""
+
+    __nb_objects = 0
+
+    def __init__(self, id=None):
+        """Class constructor for Base
+
+        Args:
+            id (int): The id for the Base instance. Defaults to None.
+        """
+        if id is not None:
+            self.id = id
+        else:
+            Base.__nb_objects += 1
+            self.id = Base.__nb_objects
