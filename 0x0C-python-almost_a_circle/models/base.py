bbd229beeeff3aca8ec066302bd3fac5f91a2642
Teminijesu Olaleye <holarxmanuel@gmail.com>
2023-09-18 22:36:54 -0700

    halloedup

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -1,4 +1,6 @@
 import json
+import csv
+import turtle
 
 
 class Base:
@@ -20,20 +22,17 @@ class Base:
     @classmethod
     def save_to_file(cls, list_objs):
         filename = cls.__name__ + ".json"
-        obj_list = []
-
-        if list_objs is not None:
-            for obj in list_objs:
-                obj_list.append(obj.to_dictionary())
-
+        if list_objs is None:
+            list_objs = []
+        list_dicts = [obj.to_dictionary() for obj in list_objs]
+        json_str = cls.to_json_string(list_dicts)
         with open(filename, mode="w", encoding="utf-8") as file:
-            file.write(cls.to_json_string(obj_list))
+            file.write(json_str)
 
     @staticmethod
     def from_json_string(json_string):
-        if json_string is None or json_string == "":
+        if json_string is None or len(json_string) == 0:
             return []
-
         return json.loads(json_string)
 
     @classmethod
@@ -48,11 +47,60 @@ class Base:
     @classmethod
     def load_from_file(cls):
         filename = cls.__name__ + ".json"
-
         try:
             with open(filename, mode="r", encoding="utf-8") as file:
-                json_string = file.read()
-                obj_list = cls.from_json_string(json_string)
-                return [cls.create(**obj) for obj in obj_list]
+                json_str = file.read()
+                list_dicts = cls.from_json_string(json_str)
+                return [cls.create(**d) for d in list_dicts]
         except FileNotFoundError:
             return []
+
+    @classmethod
+    def save_to_file_csv(cls, list_objs):
+        filename = cls.__name__ + ".csv"
+        with open(filename, mode="w", newline="") as file:
+            writer = csv.writer(file)
+            for obj in list_objs:
+                data = [getattr(obj, attr) for attr in obj.to_dictionary()]
+                writer.writerow(data)
+
+    @classmethod
+    def load_from_file_csv(cls):
+        filename = cls.__name__ + ".csv"
+        instances = []
+        with open(filename, mode="r", newline="") as file:
+            reader = csv.reader(file)
+            for row in reader:
+                instance = cls(*map(int, row))
+                instances.append(instance)
+        return instances
+
+    @staticmethod
+    def draw(list_rectangles, list_squares):
+        turtle.Screen().title("Turtle Drawing")
+        turtle.speed(0)
+        turtle.bgcolor("white")
+        turtle.penup()
+        turtle.hideturtle()
+
+        for rect in list_rectangles:
+            turtle.color("blue")
+            turtle.goto(rect.x, rect.y)
+            turtle.pendown()
+            for _ in range(2):
+                turtle.forward(rect.width)
+                turtle.left(90)
+                turtle.forward(rect.height)
+                turtle.left(90)
+            turtle.penup()
+
+        for square in list_squares:
+            turtle.color("red")
+            turtle.goto(square.x, square.y)
+            turtle.pendown()
+            for _ in range(4):
+                turtle.forward(square.size)
+                turtle.left(90)
+            turtle.penup()
+
+        turtle.exitonclick()
